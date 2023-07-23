de53bb8d1c8ed965ce703e2646606e2716209c7d
yukishi24 <yukishi24github@gmail.com>
2023-07-23 17:02:10 +0900

    modify: api

--- api/src/main/java/com/example/demo/ApiApplication.java
+++ api/src/main/java/com/example/demo/ApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiServerApplication {
+public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiServerApplication.class, args);
+		SpringApplication.run(ApiApplication.class, args);
 	}
 
 }
