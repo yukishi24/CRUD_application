8c0f4060e822402101cba750081e27870d3fc8d2
yukishi24 <yukishi24github@gmail.com>
2023-07-25 23:04:57 +0900

    create: mybatis logic
    selectの記述。
    xmlファイルのみ。
    次回Javaに着手

---
+++ api/src/main/java/com/example/demo/domain/user/model/Employee.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.example.demo.domain.user.model;
+
+/**
+ * employeeテーブルのマッピングクラス
+ * 
+ * @author yukishi
+ *
+ */
+public class Employee {
+	private int id; // id
+	private String name; // 氏名
+}
