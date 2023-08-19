43d2243ad2b3708bafc6a87278b2dff4f22ed110
yukishi24 <yukishi24github@gmail.com>
2023-08-19 23:54:01 +0900

    remove: client

--- app/client/src/pages/top/main.js
+++
@@ -1,9 +0,0 @@
-// TOP main.js
-import Vue from 'vue';
-import Top from './Top.vue';
-
-Vue.config.productionTip = false;
-
-new Vue({
-  render: h => h(Top),
-}).$mount('#app');
--- app/node.sh
+++ app/node.sh
@@ -5,6 +5,9 @@ apt install npm
 npm install -g n
 sudo n install 12.22.9
 sudo n install lts
+
 npm install -g @vue/cli
 npm install --save-dev html-webpack-plugin
 npm install preload-webpack-plugin --force 
+
+npm install webpack-cli vue-loader vue-template-compiler
